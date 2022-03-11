ed585ecd60e15666c9e6c9447018670afb94b7bb
svasenkov <aanher@gmail.com>
2022-03-11 22:06:28 +0300

    added allure listener

--- src/test/java/tests/AndroidSelenideTests.java
+++ src/test/java/tests/AndroidSelenideTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -9,6 +10,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 
+@Tag("selenide")
 public class AndroidSelenideTests extends TestBase {
     @Test
     void searchTest() {
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,19 +3,22 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import drivers.BrowserstackMobileDriver;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.Attach.getSessionId;
-import static helpers.Browserstack.videoUrl;
 
 
 public class TestBase {
     @BeforeAll
     public static void setup() {
+        addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.browser = BrowserstackMobileDriver.class.getName();
 //        Configuration.startMaximized = false;
         Configuration.browserSize = null;
