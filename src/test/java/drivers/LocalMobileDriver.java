0276a7fa0545100cb3079ad74fca25805dd5740b
svasenkov <aanher@gmail.com>
2022-03-15 21:51:28 +0300

    added test with real

--- src/test/java/drivers/LocalMobileDriver.java
+++ src/test/java/drivers/LocalMobileDriver.java
@@ -31,10 +31,13 @@ public class LocalMobileDriver implements WebDriverProvider {
         options.merge(capabilities);
         options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
         options.setPlatformName("Android");
-        options.setDeviceName("Pixel_4_API_30");
+        options.setDeviceName("RFCR90ZMNQP");
+//        options.setDeviceName("Pixel_4_API_30");
         options.setPlatformVersion("11.0");
 //        options.setCapability(APPLICATION_NAME, "Appium");
         options.setApp(app.getAbsolutePath());
+        options.setLocale("en");
+        options.setLanguage("en");
         options.setAppPackage("org.wikipedia.alpha");
         options.setAppActivity("org.wikipedia.main.MainActivity");
 
